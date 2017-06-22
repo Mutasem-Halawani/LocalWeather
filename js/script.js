ebf8bdd0983c83166d7a123a034c197ac0bd2380
mutasem <mutasem.halawani@gmail.com>
2017-06-23 00:37:28 +0300

    use promises for ajax call

--- js/script.js
+++ js/script.js
@@ -18,34 +18,48 @@ function showPosition(position) {
 }
 
 function getLocationWeather(latitude,longitude){
-$.ajax({
-    url: 'https://api.darksky.net/forecast/72a0ccbd2f9e2c583f5c84a84e55748e/' + latitude + ',' + longitude,
-    data: 'location',
-    type: 'GET',
-    crossDomain: true,
-    dataType: 'jsonp',
-    success: setData,
-    error: function(){alert('We seem to have an issue with Our location Service');}
-});
- return false;
+    $.ajax({
+            url: 'https://api.darksky.net/forecast/72a0ccbd2f9e2c583f5c84a84e55748e/' + latitude + ',' + longitude,
+            data: 'location',
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: getData,
+            error: function(){alert('We seem to have an issue with Our location Service');}
+    })
+            .done(function(){
+               getCityName();
+            })
+            .done(function(data,response){
+                showAll(data,response);
+            });
 }
-function setData(data){
-    ['temperature','summary'].forEach(v => {$(`#${v}`).text(data.currently[v]);});
-    getCityName();
+
+function getData(data){
+    console.log('success');
 }
-// Get city name by IP
+
 function getCityName(){
 $.get("https://ipinfo.io", function(response) {
-  $('#location').text(response.city);
-  if (response.country === "US") {
-       $('#temperature').append("F");
-      console.log("US");
-  }
-  else { // Convert the temprature to Celsius if user is not in the US
-      let FahrenheitTemprature = parseInt($('#temperature').text());
-      let CelsiusTemprature =  Math.round((FahrenheitTemprature - 32) * .5556) ;
-      $('#temperature').text(CelsiusTemprature + "°C");
-  }
+//    console.log(response);
+$('#location').text(response.city);
+    return(response);
 }, "jsonp");
 }
-    
+
+function showAll(data,response){
+    console.log(data);
+//    console.log(response);
+    $('#location').text(response.city);
+    ['temperature','summary'].forEach(v => {$(`#${v}`).text(data.currently[v]);});
+    $('#location').text(response.city);
+     if (response.country === "US") {
+          $('#temperature').append("F");
+          console.log("US");
+     }
+     else { // Convert the temprature to Celsius if user is not in the US
+          let FahrenheitTemprature = parseInt($('#temperature').text());
+          let CelsiusTemprature =  Math.round((FahrenheitTemprature - 32) * .5556) ;
+          $('#temperature').text(CelsiusTemprature + "°C");
+     }
+}
